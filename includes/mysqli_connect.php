0ae81216672c1613e16548cb4f061e8dd6ab5e2c
Adam Soliman <solimada41@gmail.com>
2016-12-03 18:40:00 -0500

    connection to database works. replace the username and password with your own in includes/mysqli_connect to get it working

--- includes/mysqli_connect.php
+++ includes/mysqli_connect.php
@@ -1,20 +1,21 @@
+#!/usr/local/bin/php
 <?php
 
-// Set the database access information as constants:
-DEFINE ('DB_USER', 'root');
-DEFINE ('DB_PASSWORD', 'PASSWORD');
-DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', 'NAME');
+$connection = oci_connect($username = 'username',
+                          $password = 'password',
+                          $connection_string = '//oracle.cise.ufl.edu/orcl');
+$statement = oci_parse($connection, 'SELECT * FROM users');
+oci_execute($statement);
 
-// Make the connection:
-$mysqli = new MySQLi(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-
-// Verify the connection:
-if ($mysqli->connect_error) {
-	//echo $mysqli->connect_error;
-	unset($mysqli);
-} else {
-	$mysqli->set_charset('utf8');
+$rows = array();
+while($r = oci_fetch_assoc($statement)) {
+	var_dump($r);
+	//$rows[] = $r;
 }
 
+//
+// VERY important to close Oracle Database Connections and free statements!
+//
+oci_free_statement($statement);
+oci_close($connection);
 ?>
--- index.php
+++ index.php
@@ -1,52 +1,37 @@
-<?php session_start(); ?><!doctype html>
-<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
-<!--[if IE 9]>    <html class="no-js ie9" lang="en"> <![endif]-->
-<!--[if gt IE 9]><!--> <html class="no-js" lang="en" itemscope itemtype="http://schema.org/Product"> <!--<![endif]-->
+#!/usr/local/bin/php
+
+<?php session_start(); ?><!DOCTYPE html>
+<html class="no-js" lang="en" itemscope itemtype="http://schema.org/Product">
 <head>
 	<meta charset="utf-8">
 
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
 	<title>Airport Flight Board - Login</title>
-	<meta name="description" content="" />
-	<meta name="keywords" content="" />
-	<meta name="author" content="humans.txt">
-
-	<link rel="shortcut icon" href="favicon.png" type="image/x-icon" />
-
-	<!-- Facebook Metadata /-->
-	<meta property="fb:page_id" content="" />
-	<meta property="og:image" content="" />
-	<meta property="og:description" content=""/>
-	<meta property="og:title" content=""/>
-
-	<!-- Google+ Metadata /-->
-	<meta itemprop="name" content="">
-	<meta itemprop="description" content="">
-	<meta itemprop="image" content="">
-
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
-
-	<link rel="stylesheet" href="css/gumby.css">
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="stylesheet" href="css/index.css">
-	<link href='https://fonts.googleapis.com/css?family=Lato:300,400,400italic,700' rel='stylesheet' type='text/css'>
-
-	<script src="js/libs/modernizr-2.6.2.min.js"></script>
+	<meta name="description" content="DESC" />
+	<meta name="keywords" content="KEYWRD" />
+	
 </head>
 
 <body>
 
+<form>
+Username:<br>
+<input type="text" name="username"><br>
+Password:<br>
+<input type="text" name="password"><br>
+</form>
+
+
+
 <?php
 
-$m_u = $m_p = $u_l = FALSE;
+require('includes/mysqli_connect.php');
+
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-	// Include the mysqli connection file
-	require('includes/mysqli_connect.php');
+$m_u = $m_p = $u_l = FALSE;
 
 	// Assume false values
 	$u = $p = FALSE;
@@ -109,62 +94,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	}
 
 }
-
 ?>
 
-<div class="row">
-	<div class="centered twelve columns">
-		<div class="logo-container">
-			<img src="img/logo.png" gumby-retina="" title="Airport Flight Board System"/>
-		</div>
-		<div class="row">
-			<div class="centered eight columns">
-				<div class="landing-box">
-					<h2>Sign into your account.</h2>
-					<?php if($u_l) { echo '<div class="danger alert">Your username/email and password combination is not valid. Please try again.</div>'; } ?>
-					<form id="process" action="" method="post">
-						<div class="field">
-							<?php if($m_u) { echo '<div class="danger alert">You forgot to enter your username or email address.</div>'; } ?>
-							<input type="text" class="input" name="eou" placeholder="Email or Username" autocomplete="off"/>
-						</div>
-						<div class="field">
-							<?php if($m_p) { echo '<div class="danger alert">You forgot to enter your password.</div>'; } ?>
-							<input type="password" class="input" name="pas" placeholder="Password" autocomplete="off"/>
-						</div>
-						<div class="field">
-							<div class="medium-button green">
-								<input type="submit" value="Sign In"/>
-							</div>
-						</div>
-					</form>
-					<h3>Looking to sign up for an account? Sorry, registration is closed for now.</h3>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
 
-	<script>
-	var oldieCheck = Boolean(document.getElementsByTagName('html')[0].className.match(/\soldie\s/g));
-	if(!oldieCheck) {
-	document.write('<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"><\/script>');
-	} else {
-	document.write('<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"><\/script>');
-	}
-	</script>
-	<script>
-	if(!window.jQuery) {
-	if(!oldieCheck) {
-	  document.write('<script src="js/libs/jquery-2.0.2.min.js"><\/script>');
-	} else {
-	  document.write('<script src="js/libs/jquery-1.10.1.min.js"><\/script>');
-	}
-	}
-	</script>
-
-	<script src="js/gumby.min.js"></script>
-	<script src="js/plugins.js"></script>
-	<script src="js/main.js"></script>
 
   </body>
 </html>
